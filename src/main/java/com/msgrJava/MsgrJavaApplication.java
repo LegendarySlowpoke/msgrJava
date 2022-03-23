d8724b8d2615c1da40b857d1099ae2272368f856
LegendarySlowpoke <63976295+LegendarySlowpoke@users.noreply.github.com>
2022-03-23 16:48:20 +0200

    smth new

--- src/main/java/com/msgrJava/MsgrJavaApplication.java
+++ src/main/java/com/msgrJava/MsgrJavaApplication.java
@@ -3,6 +3,7 @@ package com.msgrJava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MsgrJavaApplication {
 
---
+++ src/main/java/com/msgrJava/entities/EntityUser.java
@@ -0,0 +1,33 @@
+package com.msgrJava.entities;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "user") // for SQL table
+public class EntityUser {
+    //Fields
+    //Contacts
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Id
+    @Column(name = "tag", nullable = false)
+    private String userTAG;
+    @Id
+    @Column(name="phoneNumber", nullable = true)
+    private int phoneNumber;
+
+    //Info
+    private String name;
+    private String surname;
+    private String email;
+
+    @ElementCollection
+    private List<Long> userContacts;
+
+
+
+
+}
