692761d24fa79ec200dfafe1108a5fbd5073b1bb
LegendarySlowpoke <63976295+LegendarySlowpoke@users.noreply.github.com>
2022-03-23 19:34:01 +0200

    added new server side exception

---
+++ src/main/java/com/msgrJava/exceptions/serverSide/databaseOfflineException.java
@@ -0,0 +1,7 @@
+package com.msgrJava.exceptions.serverSide;
+
+public class databaseOfflineException extends Throwable {
+    public databaseOfflineException(String message) {
+        super(message);
+    }
+}
