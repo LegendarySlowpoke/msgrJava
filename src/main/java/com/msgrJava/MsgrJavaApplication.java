b3c6ec8ee828568d645a7e628e048c9030786a20
LegendarySlowpoke <63976295+LegendarySlowpoke@users.noreply.github.com>
2022-03-23 16:12:51 +0200

    First load

---
+++ src/main/java/com/msgrJava/MsgrJavaApplication.java
@@ -0,0 +1,14 @@
+package com.msgrJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgrJavaApplication {
+
+	public static void main(String[] args) {
+		//System.out.println(org.hibernate.Version.getVersionString());
+		SpringApplication.run(MsgrJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msgrJava/MsgrJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msgrJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgrJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
