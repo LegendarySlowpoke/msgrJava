d14a3a8479925c61fe22177511bfd06a8b115709
LegendarySlowpoke <63976295+LegendarySlowpoke@users.noreply.github.com>
2022-03-23 17:52:14 +0200

    New package 'model' was added

--- src/main/java/com/msgrJava/entities/EntityUser.java
+++ src/main/java/com/msgrJava/entities/EntityUser.java
@@ -7,26 +7,26 @@ import java.util.List;
 @Entity
 @Table(name = "user") // for SQL table
 public class EntityUser {
-    //Fields
+    //Fields---------------------------
     //Contacts
+    @Id
     @Column(name = "id", nullable = false)
     private Long id;
-    @Id
     @Column(name = "tag", nullable = false)
     private String userTAG;
-    @Id
     @Column(name="phoneNumber", nullable = true)
     private int phoneNumber;
-
-
     //Info
     private String name;
     private String surname;
     private String email;
-
+    //SecuredInfo
+    private String passHash;
     @ElementCollection
     private List<Long> userContacts;
+    //----------------------------------
 
+    public EntityUser() {}
 
 
 
---
+++ src/main/java/com/msgrJava/model/UserModel.java
@@ -0,0 +1,4 @@
+package com.msgrJava.model;
+
+public class UserModel {
+}
